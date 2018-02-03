700219bc7efcaa93c1227ea273a01fdb7fb5d035
onthir <jny.bbk1@gmail.com>
2018-02-03 09:59:44 +0545

    Whitenoise satic added

--- classk/settings.py
+++ classk/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
