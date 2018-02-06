4f01ed9b00fa587c9855edbb68691f7154b7ba23
onthir <jny.bbk1@gmail.com>
2018-02-06 09:01:31 +0545

    User Permissions and UI

--- account/views.py
+++ account/views.py
@@ -66,91 +66,96 @@ def login_user(request):
 
 # logout user
 def logout_user(request):
-    logout(request)
-    url = reverse("accounts:login")
-    return redirect(url, args=(),kwargs={})
+    if request.user.is_authenticated:
+        logout(request)
+        url = reverse("accounts:login")
+        return redirect(url, args=(),kwargs={})
+    else:
+        return redirect("accounts:login")
 
 # user profile
 
 def profile(request, user):
-    
-    global visits
-    global points
-    
-    points = []
-    visits = []
-    
-    profile = User.objects.get(username=user)
-    details = Profile.objects.get(user_id=profile.id)
+    if request.user.is_authenticated():
+        global visits
+        global points
+        
+        points = []
+        visits = []
+        
+        profile = User.objects.get(username=user)
+        details = Profile.objects.get(user_id=profile.id)
 
-    # user stats asked, answered and most recent questions
-    all_questions = Question.objects.filter(user_id=profile.id)
-    asked = Question.objects.filter(user_id=profile.id).count()
-    all_answers_by_the_user = Answer.objects.filter(user_id=profile.id)
-    answered = Answer.objects.filter(user_id=profile.id).count()
-    recent = Question.objects.filter(user_id=profile.id).order_by('-date')[0:3]
+        # user stats asked, answered and most recent questions
+        all_questions = Question.objects.filter(user_id=profile.id)
+        asked = Question.objects.filter(user_id=profile.id).count()
+        all_answers_by_the_user = Answer.objects.filter(user_id=profile.id)
+        answered = Answer.objects.filter(user_id=profile.id).count()
+        recent = Question.objects.filter(user_id=profile.id).order_by('-date')[0:3]
 
-    # increasing the profile views
-    view_count = details.viewes + 1
-    details.viewes = view_count
-    details.save() 
-    new_data = details.viewes
-    # getting recent answers by the user
-    recent_answers = Answer.objects.filter(user_id=profile.id).order_by('-posted_on')[0:3]
+        # increasing the profile views
+        view_count = details.viewes + 1
+        details.viewes = view_count
+        details.save() 
+        new_data = details.viewes
+        # getting recent answers by the user
+        recent_answers = Answer.objects.filter(user_id=profile.id).order_by('-posted_on')[0:3]
 
-    # rank of that user
-    all_profile = Profile.objects.all()
-    for p in all_profile:
-        point = p.points
-        points.append(point)
-    ranks_in_order = sorted(points)
-    highest_to_lowest =  (sorted(ranks_in_order, key=int, reverse=True))
+        # rank of that user
+        all_profile = Profile.objects.all()
+        for p in all_profile:
+            point = p.points
+            points.append(point)
+        ranks_in_order = sorted(points)
+        highest_to_lowest =  (sorted(ranks_in_order, key=int, reverse=True))
 
-    index_of_users_points = highest_to_lowest.index(details.points) 
-    length_of_list = len(points)
+        index_of_users_points = highest_to_lowest.index(details.points) 
+        length_of_list = len(points)
 
-    rank = (index_of_users_points/length_of_list)*100
-    rank_title = "Top " + str(rank)[:4] + "% with rank " + str(index_of_users_points + 1)
-    ranked = index_of_users_points + 1
+        rank = (index_of_users_points/length_of_list)*100
+        rank_title = "Top " + str(rank)[:4] + "% with rank " + str(index_of_users_points + 1)
+        ranked = index_of_users_points + 1
 
-    # highest profile visits
-    all_profile_views = Profile.objects.all()
-    for a in all_profile_views:
-        v = a.viewes
-        visits.append(v)
-    visit_sorted = sorted(visits)
-    descend = sorted(visit_sorted, key=int, reverse=True)
+        # highest profile visits
+        all_profile_views = Profile.objects.all()
+        for a in all_profile_views:
+            v = a.viewes
+            visits.append(v)
+        visit_sorted = sorted(visits)
+        descend = sorted(visit_sorted, key=int, reverse=True)
 
-    index_of_users_visits = descend.index(details.viewes) + 1
+        index_of_users_visits = descend.index(details.viewes) + 1
 
-    # satisfied answers by the user
-    all_answers = Answer.objects.filter(user_id=profile.id)
-    sat_ans = all_answers.filter(satisfied=True)
-    irrelevant_ans = all_answers.filter(satisfied=False)
+        # satisfied answers by the user
+        all_answers = Answer.objects.filter(user_id=profile.id)
+        sat_ans = all_answers.filter(satisfied=True)
+        irrelevant_ans = all_answers.filter(satisfied=False)
 
-    # profile score
-    # points
-    points = Profile.objects.get(user=request.user)
-    score = points.points
-    context = {
-        'profile': profile,
-        'details': details,
-        'asked': asked,
-        'answered': answered,
-        'recent':recent,
-        'recent_answers': recent_answers,
-        'new_data': new_data,
-        'all_questions':all_questions,
-        'rank_title':rank_title,
-        'index_of_users_points': index_of_users_points,
-        'ranked':ranked,
-        'index_of_users_visits': index_of_users_visits,
-        'sat_ans':sat_ans,
-        'irrelevant_ans':irrelevant_ans,
-        'points':points,
-        'score':score,
-    }
-    return render(request, 'account/profile.html', context)
+        # profile score
+        # points
+        points = Profile.objects.get(user=request.user)
+        score = points.points
+        context = {
+            'profile': profile,
+            'details': details,
+            'asked': asked,
+            'answered': answered,
+            'recent':recent,
+            'recent_answers': recent_answers,
+            'new_data': new_data,
+            'all_questions':all_questions,
+            'rank_title':rank_title,
+            'index_of_users_points': index_of_users_points,
+            'ranked':ranked,
+            'index_of_users_visits': index_of_users_visits,
+            'sat_ans':sat_ans,
+            'irrelevant_ans':irrelevant_ans,
+            'points':points,
+            'score':score,
+        }
+        return render(request, 'account/profile.html', context)
+    else:
+        return redirect("accounts:login")
 
 # update profile
 def update_profile(request, user):
