15499b08bfda7b907b2c200a31a40480c65e0017
onthir <bibek09@outlook.com>
2023-07-06 23:42:42 -0500

    updated new paths

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from . import views
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth
